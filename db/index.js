e6c018f0994a0bdf8edd69cb78a09653a3ce6994
KaterynaKompaniiets <katerynako@tahometer.com>
2022-08-18 11:25:47 +0300

    final version ORM

--- db/index.js
+++ db/index.js
@@ -1,3 +1,5 @@
+const { DATE } = require('sequelize');
+const { NOW } = require('sequelize');
 const  { Sequelize, Model, DataTypes } = require('sequelize');
 
 const sequelize = new Sequelize('todolist', 'todolist_app', 'intern', {
@@ -5,9 +7,9 @@ const sequelize = new Sequelize('todolist', 'todolist_app', 'intern', {
   dialect: 'postgres'
 });
 
- class Task extends Model {
+ class List extends Model {
  }
- Task.init({
+ List.init({
   id: {
     type: DataTypes.INTEGER ,
     allowNull: false,
@@ -24,33 +26,87 @@ const sequelize = new Sequelize('todolist', 'todolist_app', 'intern', {
   timestamps: false
 })
 
-async function getAllTasks(){ 
-  return await Task.findAll()
+async function getAllLists(){ 
+  return await List.findAll()
 } 
-async function createTask(listName){
-  return await Task.create({list_name: listName})
+async function createList(listName){
+  return await List.create({list_name: listName})
 }
 
-async function updateTask(listName, id) {
-  return await Task.update({ list_name: listName}, {
+async function updateList(listName, id) {
+  return await List.update({ list_name: listName}, {
     where: {
       id: id
-    }
+    },
+    returning: true
   })
 }
 
-async function deleteTask(id) {
+async function deleteList(id) {
 
-  return await Task.destroy({
+  return await List.destroy({
     where: {
       id: id
-    }
+    },
+    returning: true
   })
+}
+
+class Task extends Model {
+}
+Task.init({
+ id: {
+   type: DataTypes.INTEGER ,
+   allowNull: false,
+   primaryKey:true,
+   autoIncrement: true,
+ },
+ list_id: {
+  type: DataTypes.INTEGER ,
+  allowNull: false,
+},
+ title: {
+   type: DataTypes.STRING,
+   allowNull: false
+ },
+ description: {
+  type: DataTypes.STRING,
+  allowNull: true
+},
+done: {
+  type: DataTypes.BOOLEAN,
+  allowNull: false
+},
+due_date: {
+  type: DataTypes.DATE,
+  allowNull: false
+}
+}, {
+ sequelize,
+ modelName: 'task',
+ timestamps: false
+})
+
+List.hasMany(Task, { foreignKey: 'list_id' });
+Task.belongsTo(List, { foreignKey: 'list_id' });
+
+
+async function getCollectionToday(){
+  //pool.query('SELECT *, lists.list_name  FROM tasks LEFT JOIN lists ON lists.id=list_id WHERE due_date = \'2022-08-11\'')
+  return await Task.findAll({
+    include: [{
+      model: List,
+      
+    }],
+    where: { due_date : '2022-08-12'  } 
+  });
+
 }
 
 module.exports= {
-  getAllTasks,
-  createTask,
-  updateTask,
-  deleteTask
+  getAllLists,
+  createList,
+  updateList,
+   deleteList,
+  getCollectionToday
 } 
--- routes/index.js
+++ routes/index.js
@@ -7,31 +7,36 @@ router.get('/lists',getAllTasks)
 router.post('/lists', createTask)
 router.put('/lists/:id', updateTask)
 router.delete('/lists/:id', deleteTask)
-
+router.get('/collection/today',getCollectionToday)
 
 function getAllTasks(req,res){
-    db.getAllTasks().then(result=> res.send(result))
+    db.getAllLists().then(result=> res.send(result))
 }
 
 function createTask(req, res){
     const listName= req.body.listName 
-      db.createTask(listName).then((result)=> res.send(result))
+      db.createList(listName).then((result)=> res.send(result))
 }
 
 
 function updateTask(req,res){
     const listName= req.body.listName
     const id = parseInt(req.params.id)
-    db.updateTask(listName,id).then((result)=> res.send(result))
+    db.updateList(listName,id).then((result)=> res.send(result))
 }
 
 function deleteTask(req,res){
     const id = parseInt(req.params.id)
-    db.deleteTask(id).then((result)=> res.send(result)).catch((error) => {
-        return res.status(500).json({ message: error.message });
-      })
-    
+    db.deleteList(id).then((result)=> {res.send({
+        status: result,
+      });
+    });
 }
 
+function getCollectionToday(req,res){
+    db.getCollectionToday().then((result)=> res.send(result)).catch((error) => {
+        return res.status(500).json({ message: error.message });
+      })
+}
 
 module.exports = router
